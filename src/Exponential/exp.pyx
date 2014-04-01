b9273bc9d643dc6148619767c4ba45b96ac1aad8
TjKlug <tomjklug@gmail.com>
2014-04-01 09:01:34 -0500

    Adding Exponential Function to source

---
+++ src/Exponential/exp.pyx
@@ -0,0 +1,15 @@
+"""
+This module contains a function that allows you to do an integer raised to an integer.
+"""
+
+def Exp(int x, int y):
+    """
+Input: x , y
+x, y (integer): x is the base, y is the expontent
+Returns:
+An integer answer for the problem
+"""
+
+    
+    return pow(x, y)
+
